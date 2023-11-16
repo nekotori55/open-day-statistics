67e8498fbacd0e0b275f2dfcd4db91b3f6c0557b
nekotori55 <nekotori55@gmail.com>
2023-11-16 22:19:22 +0300

    Заглушка для бекэнда
    посылает тестовые данные по запросу

--- api/app.py
+++ api/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+import csv
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/api/data/')
+def test_data():
+    data = []
+    with open("data.csv", encoding="utf8") as source:
+        reader = csv.reader(source, delimiter=',')
+        for row in reader:
+            values = list(row)
+            data.append({'id': values[0], 'region': values[1], 'count': int(values[2])})
+    return jsonify(data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
