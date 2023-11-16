c7fe5299d8bfb9657506d2ed4264a8f0aa892cbc
Sardelnik <ssa.lolik@gmail.com>
2023-11-16 21:00:58 +0300

    Добавление главного логотипа

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,10 +1,19 @@
-
+import './Header.css'
+import bmstuLogo from '../icons/logo.png'
 
 function Header() {
     return (
-        <div>
-
-        </div>
+        <header>
+            <div className="logo">
+                <div className="logo-image">
+                    <img src={bmstuLogo} alt="Logo"/>
+                </div>
+                <span className="logo-text">
+                    <div className="logo-suptitle">Калужский филиал</div>
+                    <div className="logo-title">МГТУ имени Н.Э. Баумана</div>
+                </span>
+            </div>
+        </header>
     )
 }
 
