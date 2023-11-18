b3c57b9cd1519e59e8bb003a12342b50219b91ce
Sardelnik <ssa.lolik@gmail.com>
2023-11-18 22:03:03 +0300

    Добавление основных элементов и стилей

--- src/form/Form.js
+++ src/form/Form.js
@@ -2,8 +2,20 @@ import './Form.css'
 
 function Form() {
     return (
-        <div>
+        <div className="form-background">
+            <div className="form-title">Анкета</div>
 
+            <input type="text" className="form-select" name="region" placeholder="Область"/>
+            <input type="text" className="form-select" name="district" placeholder="Район"/>
+            <input type="text" className="form-select" name="school" placeholder="Школа"/>
+
+            <label className="parents-check">
+                <input type="checkbox" name="parents"/>
+                <span className="fake-checkbox"></span>
+                <span className="checkbox-label">Приехал с родителями?</span>
+            </label>
+
+            <button type="submit" className="form-button">Отправить</button>
         </div>
     )
 }
