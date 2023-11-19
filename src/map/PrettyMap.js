a8e19b0228d0a0e09798a360cb94eedcad9a5c33
nekotori55 <nekotori55@gmail.com>
2023-11-19 13:37:08 +0300

    Рефакторинг

--- src/map/PrettyMap.js
+++ src/map/PrettyMap.js
@@ -1,4 +1,7 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
+
+const obninskId = "kl_26";
+const bananaId = "kl_19";
 
 function PrettyMap({data, childId, children}) {
     const [currentData, setCurrentData] = useState([]);
@@ -8,18 +11,18 @@ function PrettyMap({data, childId, children}) {
     let doDrawCircles = true;
     let isLineAnimated = true;
 
+    let center = "kl_25";
+
     useEffect(() => {
-            const svgRoot = document.getElementById(childId)
+            const svgElement = document.getElementById(childId);
 
             let drawData = convertRawDataToDrawData(data);
 
-            removePreviouslyRenderedElements(svgRoot);
+            removePreviouslyRenderedElements(svgElement);
 
             if (doDrawLines) {
-                let lineTargetId = "kl_25"
-                let lineTargetElement = svgRoot.getElementById(lineTargetId)
-
-                drawLines(drawData, lineTargetElement, isLineAnimated, svgRoot);
+                drawLines(drawData, center, isLineAnimated, svgElement);
+            }
             }
 
             if (doDrawCircles) {
@@ -28,75 +31,113 @@ function PrettyMap({data, childId, children}) {
             if (doDrawNumbers) {
             }
 
-            setCurrentData(drawData)
+            setCurrentData(drawData);
         }
         ,
         [data]
-    )
+    );
 
     return (children);
 }
 
-function drawLines(newDrawData, targetElement, isLineAnimated, svgRoot) {
-    let lineTargetRect = targetElement.getBBox()
+function calcElementPosition(regionRect, regionId = "") {
+    let regionRectCenter = {
+        x: regionRect.x + regionRect.width / 2,
+        y: regionRect.y + regionRect.height / 2
+    };
 
-    for (const obj of newDrawData) {
-        let regionId = obj.id
-        let count = obj.count
+    //TODO: Hardcoded ID, убрать по возможности (или хотя бы исправить)
+    if (regionId === bananaId) {
+        regionRectCenter.x += 25;
+    }
+    if (regionId === obninskId) {
+        regionRectCenter.x += 70;
+        regionRectCenter.y -= 30;
+    }
 
-        let regionElement = document.getElementById(regionId);
+    return regionRectCenter;
+}
+
+function createLineElement(regionId, regionRectCenter, targetRectCenter, isLineAnimated = false, animationSpeed = 1, lineColor = "#176dea") {
+    let lineElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    // s.setAttribute('dominant-baseline', 'middle')
+    lineElement.setAttribute("id", regionId + "_line");
+    lineElement.setAttribute("stroke", lineColor);
+    lineElement.setAttribute("x1", regionRectCenter.x);
+    lineElement.setAttribute("y1", regionRectCenter.y);
+    lineElement.setAttribute("x2", targetRectCenter.x);
+    lineElement.setAttribute("y2", targetRectCenter.y);
+    // lineElement.setAttribute('class', 'rendered')
+    lineElement.classList.add("rendered");
+
+    if (isLineAnimated) {
+        lineElement.setAttribute("stroke-dasharray", "40 10");
+        // lineElement.setAttribute('stroke-dasharray', '300 600')
+
+        let anim = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+
+        anim.setAttribute("attributeName", "stroke-dashoffset");
+        anim.setAttribute("values", "100%; 0%");
+        anim.setAttribute("dur", Math.floor(100 / animationSpeed) + "s");
+        // anim.setAttribute('dur', '10s')
+        anim.setAttribute("repeatCount", "indefinite");
+        anim.setAttribute("fill", "freeze");
+        anim.classList.add("rendered");
+        lineElement.append(anim);
+    }
+    return lineElement;
+}
+
+function drawLines(drawData, targetId, isLineAnimated, root) {
+    let targetElement = root.getElementById(targetId);
+    let lineTargetRect = targetElement.getBBox();
+
+    let targetRectCenter = {
+        x: lineTargetRect.x + lineTargetRect.width / 2,
+        y: lineTargetRect.y + lineTargetRect.height / 2
+    };
+
+    for (const obj of drawData) {
+        let regionId = obj.id;
+        let count = obj.count;
+
+        let regionElement = root.getElementById(regionId);
         if (regionElement != null) {
             if (regionElement !== targetElement && count !== 0) {
-                let regionRect = regionElement.getBBox()
+                let regionRect = regionElement.getBBox();
 
-                let lineElement = document.createElementNS("http://www.w3.org/2000/svg", "line")
-                // s.setAttribute('dominant-baseline', 'middle')
-                lineElement.setAttribute('id', regionId + '_line')
-                lineElement.setAttribute('stroke', '#176dea')
-                lineElement.setAttribute('x1', regionRect.x + regionRect.width / 2)
-                lineElement.setAttribute('y1', regionRect.y + regionRect.height / 2)
-                lineElement.setAttribute('x2', lineTargetRect.x + lineTargetRect.width / 2)
-                lineElement.setAttribute('y2', lineTargetRect.y + lineTargetRect.height / 2)
-                // lineElement.setAttribute('class', 'rendered')
-                lineElement.classList.add("rendered")
+                let regionRectCenter;
+                if (regionId === bananaId)
+                    regionRectCenter = calcElementPosition(regionRect, regionId);
+                else
+                    regionRectCenter = calcElementPosition(regionRect);
 
+                let lineElement = createLineElement(regionId,
+                    regionRectCenter,
+                    targetRectCenter,
+                    isLineAnimated,
+                    count);
 
-                if (isLineAnimated) {
-                    lineElement.setAttribute('stroke-dasharray', '40 10')
-                    // lineElement.setAttribute('stroke-dasharray', '300 600')
-
-                    let anim = document.createElementNS("http://www.w3.org/2000/svg", "animate")
-
-                    anim.setAttribute('attributeName', 'stroke-dashoffset')
-                    anim.setAttribute('values', '100%; 0%')
-                    anim.setAttribute('dur', Math.floor(100 / count) + 's')
-                    // anim.setAttribute('dur', '10s')
-                    anim.setAttribute('repeatCount', 'indefinite')
-                    anim.setAttribute('fill', 'freeze')
-                    anim.classList.add("rendered")
-                    lineElement.append(anim)
-
-                }
-                svgRoot.append(lineElement)
+                root.append(lineElement);
             }
         }
     }
 }
 
 function convertRawDataToDrawData(data) {
-    let drawData = []
+    let drawData = [];
     for (const [id, rawData] of Object.entries(data)) {
-        if (rawData['count'] !== 0) {
-            drawData.push({id: rawData['id'], count: rawData['count']})
+        if (rawData["count"] !== 0) {
+            drawData.push({id: rawData["id"], count: rawData["count"]});
         }
     }
     return drawData;
 }
 
-function removePreviouslyRenderedElements(svgRoot) {
-    let renderedElements = svgRoot.getElementsByClassName('rendered')
+function removePreviouslyRenderedElements(root) {
+    let renderedElements = root.getElementsByClassName("rendered");
     while (renderedElements[0]) {
-        renderedElements[0].parentNode.removeChild(renderedElements[0])
+        renderedElements[0].parentNode.removeChild(renderedElements[0]);
     }
 }
 
