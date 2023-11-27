b0e9b0f46de1ff4320756518f08dc4fa32718314
Sardelnik <ssa.lolik@gmail.com>
2023-11-27 03:38:02 +0300

    Реализован функционал

--- src/select/Select.js
+++ src/select/Select.js
@@ -1,9 +1,35 @@
 import "./Select.css"
+import {useState} from "react";
 
-function Select() {
+function Select(props) {
+
+    const [value , setValue] = useState('');
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    const compare = (item) => {
+        const searchText = value.toLowerCase();
+        const searchData = item.region.toLowerCase();
+
+        return searchText && searchData.startsWith(searchText) && searchText !== searchData;
+    }
+
+    const onSelect = (selected) => {
+        setValue(selected);
+    }
+
     return(
-        <div>
-
+        <div className="select">
+            <input type="text" value={value} onChange={onChange} />
+            <div className="dropdown">
+                {props.data.filter(compare).map((item) => (
+                    <div key={item.id} className="dropdown-raw" onClick={() => onSelect(item.region)}>
+                        {item.region}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
