37f3241006359f151f4507ce0a49663f4b4a329e
nekotori55 <nekotori55@gmail.com>
2023-12-07 10:05:12 +0300

    Метод для получения всех данных одним запросом

--- api/app.py
+++ api/app.py
@@ -3,6 +3,11 @@ import csv
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/api/data/')
 def test_data():
     data = []
@@ -11,6 +16,24 @@ def test_data():
         for row in reader:
             values = list(row)
             data.append({'id': values[0], 'region': values[1], 'count': int(values[2])})
+def get_data():
+    data = {}
+
+    conn = get_db_connection()
+    regions = conn.execute('SELECT region_id as id, region_name as name FROM regions').fetchall()
+    regions = [dict(row) for row in regions]
+    data['regions'] = regions
+
+    districts = conn.execute('SELECT district_id as id, district_name as name FROM district').fetchall()
+    districts = [dict(row) for row in districts]
+    data['districts'] = districts
+
+    schools = conn.execute('SELECT school_id as id, school_name as name FROM schools').fetchall()
+    schools = [dict(row) for row in schools]
+    data['schools'] = schools
+
+    conn.close()
+
     return jsonify(data)
 
 
