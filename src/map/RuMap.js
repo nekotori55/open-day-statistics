f51d800ad4ea3a80bf02b671253c7559f319be61
nekotori55 <nekotori55@gmail.com>
2023-12-08 21:21:29 +0300

    Анархия №8 (Фиксики снова в деле)

--- src/map/RuMap.js
+++ src/map/RuMap.js
@@ -4,7 +4,7 @@ function RussiaSVG(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
+      id="rus_map"
       version="1.2"
       viewBox="0 0 105 110"
     >
